c7c33a4de648155ade74d747fc20dffe8f6a72cc
Ty_1991 <67247243+Ty1991twitter@users.noreply.github.com>
2022-10-20 10:02:18 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,19 @@
 package;
 
 import flixel.FlxState;
+import flixel.FlxG;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
+	var text:FlxText;
 	override public function create()
 	{
 		super.create();
+		text = new FlxText(0,0,FlxG.width, "Hello World", 64);
+		text.setFormat(null,64,FlxColor.RED,FlxTextAllign.CENTER);
+		add(text);
 	}
 
 	override public function update(elapsed:Float)
